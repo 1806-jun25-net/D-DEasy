2b170abd383e8329d44785a65b20b4ac06385f91
SHADOW <angel.luis79@yahoo.com>
2018-07-30 16:32:10 -0400

    Trying to get authorized access

--- Services/DandDEasy.Services/Controllers/UserController.cs
+++ Services/DandDEasy.Services/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using DandDEasy.Services.Repo;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DandDEasy.Services.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
@@ -21,7 +22,15 @@ namespace DandDEasy.Services.Controllers
 
         }
 
-
+        
+        [HttpGet]
+        [AllowAnonymous]
+        //[Authorize]
+        public IEnumerable<User> GetUserTable()
+        {
+            var user = Repo.GetUsertable();
+            return user;
+        }
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
