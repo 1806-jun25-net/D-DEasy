ed1d13ce03cc67d7c19fd910cfc509d8fe7c3615
SHADOW <angel.luis79@yahoo.com>
2018-08-01 14:20:37 -0400

    Login, MainPage without Register full

--- DandDEasy.Project2/DandDEasy_WEB/Controllers/CampaignGraveyardsController.cs
+++ DandDEasy.Project2/DandDEasy_WEB/Controllers/CampaignGraveyardsController.cs
@@ -11,155 +11,155 @@ namespace DandDEasy_WEB.Controllers
 {
     public class CampaignGraveyardsController : Controller
     {
-        private readonly DnDEasyContext _context;
+        //private readonly DnDEasyContext _context;
 
-        public CampaignGraveyardsController(DnDEasyContext context)
-        {
-            _context = context;
-        }
+        //public CampaignGraveyardsController(DnDEasyContext context)
+        //{
+        //    _context = context;
+        //}
 
-        // GET: CampaignGraveyards
-        public async Task<IActionResult> Index()
-        {
-            var dnDContext = _context.CampaignGraveyard.Include(c => c.Campaign).Include(c => c.Character);
-            return View(await dnDContext.ToListAsync());
-        }
+        //// GET: CampaignGraveyards
+        //public async Task<IActionResult> Index()
+        //{
+        //    var dnDContext = _context.CampaignGraveyard.Include(c => c.Campaign).Include(c => c.Character);
+        //    return View(await dnDContext.ToListAsync());
+        //}
 
-        // GET: CampaignGraveyards/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: CampaignGraveyards/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var campaignGraveyard = await _context.CampaignGraveyard
-                .Include(c => c.Campaign)
-                .Include(c => c.Character)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (campaignGraveyard == null)
-            {
-                return NotFound();
-            }
+        //    var campaignGraveyard = await _context.CampaignGraveyard
+        //        .Include(c => c.Campaign)
+        //        .Include(c => c.Character)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (campaignGraveyard == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(campaignGraveyard);
-        }
+        //    return View(campaignGraveyard);
+        //}
 
-        // GET: CampaignGraveyards/Create
-        public IActionResult Create()
-        {
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password");
-            ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id");
-            return View();
-        }
+        //// GET: CampaignGraveyards/Create
+        //public IActionResult Create()
+        //{
+        //    ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password");
+        //    ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id");
+        //    return View();
+        //}
 
-        // POST: CampaignGraveyards/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,CampaignId,CharacterId")] CampaignGraveyard campaignGraveyard)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(campaignGraveyard);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", campaignGraveyard.CampaignId);
-            ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id", campaignGraveyard.CharacterId);
-            return View(campaignGraveyard);
-        }
+        //// POST: CampaignGraveyards/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,CampaignId,CharacterId")] CampaignGraveyard campaignGraveyard)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(campaignGraveyard);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", campaignGraveyard.CampaignId);
+        //    ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id", campaignGraveyard.CharacterId);
+        //    return View(campaignGraveyard);
+        //}
 
-        // GET: CampaignGraveyards/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: CampaignGraveyards/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var campaignGraveyard = await _context.CampaignGraveyard.FindAsync(id);
-            if (campaignGraveyard == null)
-            {
-                return NotFound();
-            }
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", campaignGraveyard.CampaignId);
-            ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id", campaignGraveyard.CharacterId);
-            return View(campaignGraveyard);
-        }
+        //    var campaignGraveyard = await _context.CampaignGraveyard.FindAsync(id);
+        //    if (campaignGraveyard == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", campaignGraveyard.CampaignId);
+        //    ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id", campaignGraveyard.CharacterId);
+        //    return View(campaignGraveyard);
+        //}
 
-        // POST: CampaignGraveyards/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,CampaignId,CharacterId")] CampaignGraveyard campaignGraveyard)
-        {
-            if (id != campaignGraveyard.Id)
-            {
-                return NotFound();
-            }
+        //// POST: CampaignGraveyards/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,CampaignId,CharacterId")] CampaignGraveyard campaignGraveyard)
+        //{
+        //    if (id != campaignGraveyard.Id)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(campaignGraveyard);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CampaignGraveyardExists(campaignGraveyard.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", campaignGraveyard.CampaignId);
-            ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id", campaignGraveyard.CharacterId);
-            return View(campaignGraveyard);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(campaignGraveyard);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!CampaignGraveyardExists(campaignGraveyard.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", campaignGraveyard.CampaignId);
+        //    ViewData["CharacterId"] = new SelectList(_context.Character, "Id", "Id", campaignGraveyard.CharacterId);
+        //    return View(campaignGraveyard);
+        //}
 
-        // GET: CampaignGraveyards/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: CampaignGraveyards/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var campaignGraveyard = await _context.CampaignGraveyard
-                .Include(c => c.Campaign)
-                .Include(c => c.Character)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (campaignGraveyard == null)
-            {
-                return NotFound();
-            }
+        //    var campaignGraveyard = await _context.CampaignGraveyard
+        //        .Include(c => c.Campaign)
+        //        .Include(c => c.Character)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (campaignGraveyard == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(campaignGraveyard);
-        }
+        //    return View(campaignGraveyard);
+        //}
 
-        // POST: CampaignGraveyards/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var campaignGraveyard = await _context.CampaignGraveyard.FindAsync(id);
-            _context.CampaignGraveyard.Remove(campaignGraveyard);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //// POST: CampaignGraveyards/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var campaignGraveyard = await _context.CampaignGraveyard.FindAsync(id);
+        //    _context.CampaignGraveyard.Remove(campaignGraveyard);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
-        private bool CampaignGraveyardExists(int id)
-        {
-            return _context.CampaignGraveyard.Any(e => e.Id == id);
-        }
+        //private bool CampaignGraveyardExists(int id)
+        //{
+        //    return _context.CampaignGraveyard.Any(e => e.Id == id);
+        //}
     }
 }
--- DandDEasy.Project2/DandDEasy_WEB/Controllers/CharactersController.cs
+++ DandDEasy.Project2/DandDEasy_WEB/Controllers/CharactersController.cs
@@ -11,45 +11,25 @@ namespace DandDEasy_WEB.Controllers
 {
     public class CharactersController : Controller
     {
-        private readonly DnDEasyContext _context;
-
-        public CharactersController(DnDEasyContext context)
-        {
-            _context = context;
-        }
-
         // GET: Characters
         public async Task<IActionResult> Index()
         {
-            var dnDContext = _context.Character.Include(c => c.Campaign).Include(c => c.User);
-            return View(await dnDContext.ToListAsync());
+           
+            return View();
         }
 
         // GET: Characters/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var character = await _context.Character
-                .Include(c => c.Campaign)
-                .Include(c => c.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (character == null)
-            {
-                return NotFound();
-            }
 
-            return View(character);
+            return View();
         }
 
         // GET: Characters/Create
         public IActionResult Create()
         {
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password");
-            ViewData["UserId"] = new SelectList(_context.User, "Id", "Email");
+
             return View();
         }
 
@@ -60,33 +40,33 @@ namespace DandDEasy_WEB.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,UserId,CampaignId,CreationDate,Name,Race,Background,Alignment,Experience,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,HitPoints,ArmorClass")] Character character)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(character);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", character.CampaignId);
-            ViewData["UserId"] = new SelectList(_context.User, "Id", "Email", character.UserId);
-            return View(character);
+            //if (ModelState.IsValid)
+            //{
+            //    _context.Add(character);
+            //    await _context.SaveChangesAsync();
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", character.CampaignId);
+            //ViewData["UserId"] = new SelectList(_context.User, "Id", "Email", character.UserId);
+            return View();
         }
 
         // GET: Characters/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var character = await _context.Character.FindAsync(id);
-            if (character == null)
-            {
-                return NotFound();
-            }
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", character.CampaignId);
-            ViewData["UserId"] = new SelectList(_context.User, "Id", "Email", character.UserId);
-            return View(character);
+            //var character = await _context.Character.FindAsync(id);
+            //if (character == null)
+            //{
+            //    return NotFound();
+            //}
+            //ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", character.CampaignId);
+            //ViewData["UserId"] = new SelectList(_context.User, "Id", "Email", character.UserId);
+            return View();
         }
 
         // POST: Characters/Edit/5
@@ -96,54 +76,54 @@ namespace DandDEasy_WEB.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,CampaignId,CreationDate,Name,Race,Background,Alignment,Experience,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,HitPoints,ArmorClass")] Character character)
         {
-            if (id != character.Id)
-            {
-                return NotFound();
-            }
+            //if (id != character.Id)
+            //{
+            //    return NotFound();
+            //}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(character);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CharacterExists(character.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", character.CampaignId);
-            ViewData["UserId"] = new SelectList(_context.User, "Id", "Email", character.UserId);
-            return View(character);
+            //if (ModelState.IsValid)
+            //{
+            //    try
+            //    {
+            //        _context.Update(character);
+            //        await _context.SaveChangesAsync();
+            //    }
+            //    catch (DbUpdateConcurrencyException)
+            //    {
+            //        if (!CharacterExists(character.Id))
+            //        {
+            //            return NotFound();
+            //        }
+            //        else
+            //        {
+            //            throw;
+            //        }
+            //    }
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //ViewData["CampaignId"] = new SelectList(_context.Campaign, "Id", "Password", character.CampaignId);
+            //ViewData["UserId"] = new SelectList(_context.User, "Id", "Email", character.UserId);
+            return View();
         }
 
         // GET: Characters/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var character = await _context.Character
-                .Include(c => c.Campaign)
-                .Include(c => c.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (character == null)
-            {
-                return NotFound();
-            }
+            //var character = await _context.Character
+            //    .Include(c => c.Campaign)
+            //    .Include(c => c.User)
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+            //if (character == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return View(character);
+            return View();
         }
 
         // POST: Characters/Delete/5
@@ -151,15 +131,17 @@ namespace DandDEasy_WEB.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var character = await _context.Character.FindAsync(id);
-            _context.Character.Remove(character);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            //var character = await _context.Character.FindAsync(id);
+            //_context.Character.Remove(character);
+            //await _context.SaveChangesAsync();
+            //return RedirectToAction(nameof(Index));
+            return View();
         }
 
-        private bool CharacterExists(int id)
-        {
-            return _context.Character.Any(e => e.Id == id);
-        }
+        //private bool CharacterExists(int id)
+        //{
+        //    //return _context.Character.Any(e => e.Id == id);
+
+        //}
     }
 }
