4be0a2ff1f19bc5767c0428cc0509a4f3df48aa0
RoyMamba0329 <roy.mamba0329@gmail.com>
2018-08-01 14:08:15 -0400

    minor changes to startup

--- Services/DandDEasy.Services/Startup.cs
+++ Services/DandDEasy.Services/Startup.cs
@@ -93,10 +93,10 @@ namespace DandDEasy.Services
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            app.UseDeveloperExceptionPage();
+            //}
 
             app.UseAuthentication();
             app.UseMvc();
